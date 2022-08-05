2d81a36a6b62a5796996425c41664a65b055f325
Jays-Mar <100965369+Jays-Mar@users.noreply.github.com>
2022-08-05 01:58:59 -0430

    fetch

--- src/Vistas/Login.jsx
+++ src/Vistas/Login.jsx
@@ -1,47 +1,83 @@
 import React from 'react'
-import Nav from '../Componentes/sideBar'
-import { /*BrowserRouter as Router,*/  Route, Routes, Link} from "react-router-dom";
+//import Nav from '../Componentes/sideBar'
+import { /*BrowserRouter as Router,  Route, Routes,*/ Link} from "react-router-dom";
+
+const state = { dataj: [] };
+const imprimir = () => {
+    console.log("funcion")
+}
+
+const jugadores = () => {
+    fetch(`https://localhost:3000/jugador`)
+     .then((response) => response.json())
+     .then((actualData) => console.log(actualData))
+     .catch((err) => {
+      console.log(err.message);
+     });
+}
+
+const jug = () => {
+    fetch('https://httpbin.org/post', { method: 'POST', body: 'a=1' })
+        .then(res => res.json()) // expecting a json response
+        .then(json => {
+           console.log(json)
+           //window.location.href = data.redirect;
+          })
+          .then(dataj => this.setState({ dataj }))
+          .then(console.log(this.dataj));
+}
+
+const getJugadores = (() => {
+    fetch(`http://localhost:5000/jugador`)
+     .then((response) => response.json())
+     .then((actualData) => console.log(actualData))
+     .catch((err) => {
+      console.log(err.message);
+     });
+   }, []);
+
+//onClick={console.log("click en login")}
 
 const login = () => 
 
 
 
-<div class="hero is-primary is-fullheight">
-<div class="hero-body">
+<div className="hero is-primary is-fullheight">
+<div className="hero-body">
     
-    <div class="container">
+    <div className="container">
        
-        <div class="columns is-centered">
-            <div class="column is-4">
-                <form action="" class="box">
-                    <div class="field">
-                        <label class="label">
+        <div className="columns is-centered">
+            <div className="column is-4">
+                <form action="" className="box">
+                    <div className="field">
+                        <label className="label">
                             Email
                         </label>
-                        <div class="control has-icons-left">
-                            <input type="email" class="input" placeholder="john@example.com"/>
-                            <span class="icon is-small is-left">
-                                <i class="fa fa-envelope"></i>
+                        <div className="control has-icons-left">
+                            <input type="email" className="input" placeholder="john@example.com"/>
+                            <span className="icon is-small is-left">
+                                <i className="fa fa-envelope"></i>
                             </span>
                         </div>
                     </div>
-                    <div class="field">
-                        <label class="label">Password</label>
-                        <div class="control has-icons-left">
-                            <input type="password" class="input" placeholder="john@example.com"/>
-                            <span class="icon is-small is-left">
-                                <i class="fa fa-lock"></i>
+                    <div className="field">
+                        <label className="label">Password</label>
+                        <div className="control has-icons-left">
+                            <input type="password" className="input" placeholder="john@example.com"/>
+                            <span className="icon is-small is-left">
+                                <i className="fa fa-lock"></i>
                             </span>
                         </div>
                     </div>
-                    <div class="field">
-                        <label class="label">
-                            <input type="checkbox" class="checkbox"/>
+                    <div className="field">
+                        <label className="label">
+                            <input type="checkbox" className="checkbox"/>
                             Remember me
                         </label>
                     </div>
-                    <div class="field">
-                        <button class="button is-success">Login</button>
+                    <div className="field">
+                        <button className="button is-success" onClick={jugadores}>Login</button>
                     </div>
                 </form>
             </div>
